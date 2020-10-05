5c7954ed970b7eb4f88419c1fa9220b230192a57
yinyoung090 <yinyoung090@gmail.com>
2020-10-05 23:36:25 +0800

    1005

--- ch5.py
+++ ch5.py
@@ -777,3 +777,115 @@ plt.axis([0, svm_clf.n_epochs, 0 ,100])
 
 #in[40]
 print(svm_clf.intercept_, svm_clf.coef_)
+
+#in[41]
+svm_clf2 = SVC(kernel="linear", C=C)
+svm_clf2.fit(X, y.ravel())
+print(svm_clf2.intercept_, svm_clf2.coef_)
+
+#in[42]
+yr = y.ravel()
+plt.figure(figsize=(12,3.2))
+plt.subplot(121)
+plt.plot(X[:, 0][yr==1], X[:, 1][yr==1], "g^", label="Iris-Virginica")
+plt.plot(X[:, 0][yr==0], X[:, 1][yr==0], "bs", label="Not Iris-Virginica")
+plot_svc_decision_boundary(svm_clf, 4, 6)
+plt.xlabel("Petal length", fontsize=14)
+plt.ylabel("Petal width", fontsize=14)
+plt.title("MyLinearSVC", fontsize=14)
+plt.axis([4, 6, 0.8, 2.8])
+
+plt.subplot(122)
+plt.plot(X[:, 0][yr==1], X[:, 1][yr==1], "g^")
+plt.plot(X[:, 0][yr==0], X[:, 1][yr==0], "bs")
+plot_svc_decision_boundary(svm_clf2, 4, 6)
+plt.xlabel("Petal length", fontsize=14)
+plt.title("SVC", fontsize=14)
+plt.axis([4, 6, 0.8, 2.8])
+
+#in[43]
+
+from sklearn.linear_model import SGDClassifier
+
+sgd_clf = SGDClassifier(loss="hinge", alpha = 0.017, max_iter = 50, tol=-np.infty, random_state=42)
+sgd_clf.fit(X, y.ravel())
+
+m = len(X)
+t = y * 2 - 1  # -1 if t==0, +1 if t==1
+X_b = np.c_[np.ones((m, 1)), X]  # Add bias input x0=1
+X_b_t = X_b * t
+sgd_theta = np.r_[sgd_clf.intercept_[0], sgd_clf.coef_[0]]
+print(sgd_theta)
+support_vectors_idx = (X_b_t.dot(sgd_theta) < 1).ravel()
+sgd_clf.support_vectors_ = X[support_vectors_idx]
+sgd_clf.C = C
+
+plt.figure(figsize=(5.5,3.2))
+plt.plot(X[:, 0][yr==1], X[:, 1][yr==1], "g^")
+plt.plot(X[:, 0][yr==0], X[:, 1][yr==0], "bs")
+plot_svc_decision_boundary(sgd_clf, 4, 6)
+plt.xlabel("Petal length", fontsize=14)
+plt.ylabel("Petal width", fontsize=14)
+plt.title("SGDClassifier", fontsize=14)
+plt.axis([4, 6, 0.8, 2.8])
+
+#in[44]
+from sklearn import datasets
+
+iris = datasets.load_iris()
+X = iris["data"][:,(2, 3)] # petal length, petal width
+y = iris["target"]
+
+setosa_or_versicolor = (y == 0) | ( y ==1)
+X = X[setosa_or_versicolor]
+y = y[setosa_or_versicolor]
+
+#in[45]
+from sklearn.svm import SVC, LinearSVC
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import StandardScaler
+
+C = 5
+alpha = 1/ ( C*len(X))
+
+lin_clf = LinearSVC(loss="hinge", C=C, random_state=42)
+svm_clf = SVC(kernel="linear", C=C)
+sgd_clf = SGDClassifier(loss= "hinge", learning_rate="constant", eta0=0.001, alpha=alpha,
+                        max_iter=100000, tol=-np.infty, random_state=42)
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+lin_clf.fit(X_scaled, y)
+svm_clf.fit(X_scaled, y)
+sgd_clf.fit(X_scaled, y)
+print("LinearSVC:                 ", lin_clf.intercept_, lin_clf.coef_)
+print("SVC:                       ", svm_clf.intercept_, svm_clf.coef_)
+print("SGDClassifier(alpha={:.5f})".format(sgd_clf.alpha),sgd_clf.intercept_, sgd_clf.coef_)
+
+#in[46]
+#compute the slope and bias of each decision boundary
+w1 = -lin_clf.coef_[0,0]/lin_clf.coef_[0,1]
+b1 = -lin_clf.intercept_[0]/lin_clf.coef_[0,1]
+w2 = -svm_clf.coef_[0,0]/svm_clf.coef_[0,1]
+b2 = -svm_clf.intercept_[0]/svm_clf.coef_[0,1]
+w3 = -sgd_clf.coef_[0,0]/sgd_clf.coef_[0,1]
+b3 = -sgd_clf.intercept_[0]/sgd_clf.coef_[0,1]
+
+#transform the decision boundary lines back to the original scale
+line1 = scaler.inverse_transform([[-10, -10*w1 + b1], [10, 10*w1 + b1]])
+line2 = scaler.inverse_transform([[-10, -10*w2 + b2], [10, 10*w2 + b2]])
+line3 = scaler.inverse_transform([[-10, -10*w3 + b3], [10, 10*w3 + b3]])
+
+#plot all three decision bounderies
+plt.figure(figsize=(11,4))
+plt.plot(line1[:,0], line1[:,1], "k--", label="LinearSVC")
+plt.plot(line2[:,0], line2[:,1], "b--", linewidth=2, label="SVC")
+plt.plot(line3[:,0], line3[:,1], "r--", label="SGDClassifier")
+plt.plot(X[:,0][y==1], X[:,1][y==1], "bs") # label="Iris-Versicolor"
+plt.plot(X[:,0][y==0], X[:,1][y==0], "yo") #label="Iris-Setosa"
+plt.xlabel("Petal length", fontsize=14)
+plt.ylabel("Petal width", fontsize=14)
+plt.legend(loc="upper center", fontsize=14)
+plt.axis([0, 5.5, 0, 2])
+
+plt.show()
