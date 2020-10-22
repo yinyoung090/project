ac45ca356fb40f9cd87c1395f2d09bcd5f7b0a3c
yinyoung090 <yinyoung090@gmail.com>
2020-10-22 23:28:15 +0800

    1022

--- ch5.py
+++ ch5.py
@@ -749,7 +749,7 @@ class MyLinearSVC(BaseEstimator):
             J = 1/2 * np.sum(w * w) + self.C * (np.sum(1 - X_t_sv.dot(w)) - b * np.sum(t_sv))
             self.Js.append(J)
 
-            w_gradient_vector = w - self.C* np.sum(x_t_sv, axis=0).reshape(-1, 1)
+            w_gradient_vector = w - self.C* np.sum(X_t_sv, axis=0).reshape(-1, 1)
             b_derivative = -C * np.sum(t_sv)
 
             w = w - self.eta(epoch) * w_gradient_vector
@@ -889,3 +889,110 @@ plt.legend(loc="upper center", fontsize=14)
 plt.axis([0, 5.5, 0, 2])
 
 plt.show()
+
+#in[47]
+from sklearn.datasets import fetch_openml
+mnist = fetch_openml('mnist_784', version=1, cache=True)
+
+X = mnist["data"]
+y = mnist["target"].astype(np.uint8)
+
+X_train = X[:6000]
+y_train = y[:6000]
+X_test = X[6000:]
+y_test = y[6000:]
+
+
+#in[48]
+lin_clf = LinearSVC(random_state=42)
+lin_clf.fit(X_train, y_train)
+
+#in[49]
+from sklearn.metrics import accuracy_score
+y_pred = lin_clf.predict(X_train)
+accuracy_score(y_train, y_pred)
+
+#in[50]
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train.astype(np.float32))
+X_test_scaled = scaler.fit_transform(X_test.astype(np.float32))
+
+#in[51]
+lin_clf = LinearSVC(random_state=42)
+lin_clf.fit(X_train_scaled, y_train)
+
+#in[52]
+y_pred = lin_clf.predict(X_test_scaled)
+accuracy_score(y_train, y_pred)
+
+#in[53]
+y_pred = lin_clf.predict(X_train_scaled)
+accuracy_score(y_train, y_pred)
+
+#in[54]
+svm_clf = SVC(decision_function_shape="ovr", gamma="auto")
+svm_clf.fit(X_train_scaled[:1000], y_train[:1000])
+
+#in[55]
+y_pred = svm_clf.predict(X_train_scaled)
+accuracy_score(y_train, y_pred)
+
+#train a SVM regressor on the Californial housing dataset
+#in[62]
+from sklearn.datasets import fetch_california_housing
+
+housing = fetch_california_housing()
+X = housing["data"]
+y = housing["target"]
+
+#in[63]
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+#in[64]
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+#in[65]
+from sklearn.svm import LinearSVR
+
+lin_svr = LinearSVR(random_state=42)
+lin_svr.fit(X_train_scaled, y_train)
+
+#in[66]
+from sklearn.metrics import mean_squared_error
+
+y_pred = lin_svr.predict(X_train_scaled)
+mse = mean_squared_error(y_train, y_pred)
+mse
+
+#show RMSE
+#in[67]
+np.sqrt(mse)
+
+#in[68]
+from sklearn.svm import SVR
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import reciprocal, uniform
+
+param_distributions = {"gamma":reciprocal(0.001, 0.1), "C":uniform(1, 10)}
+rnd_search_cv  = RandomizedSearchCV(SVR(), param_distributions, n_iter=10, verbose=2, cv=3, 
+    random_state=42)
+rnd_search_cv.fit(X_train_scaled, y_train)
+
+#in[69]
+rnd_search_cv.best_estimator_
+
+#in[70]
+y_pred = rnd_search_cv.best_estimator_.predict(X_train_scaled)
+mse = mean_squared_error(y_train, y_pred)
+np.sqrt(mse)
+
+#in[71]
+y_pred = rnd_search_cv.best_estimator_.predict(X_test_scaled)
+mse = mean_squared_error(y_test, y_pred)
+np.sqrt(mse)
